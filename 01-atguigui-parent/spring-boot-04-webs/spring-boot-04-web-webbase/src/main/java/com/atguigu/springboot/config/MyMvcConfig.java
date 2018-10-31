8e17e5b331b2a8b6e69000379464f052b544fe82
DopeMax <amingccc@gmail.com>
2018-10-31 17:54:07 +0800

    crud实验项目第一天

--- 01-atguigui-parent/spring-boot-04-webs/spring-boot-04-web-webbase/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ 01-atguigui-parent/spring-boot-04-webs/spring-boot-04-web-webbase/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,36 +1,17 @@
 package com.atguigu.springboot.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * @author Chen
- * @created 2018-10-31-10:39.
- */
-
 //使用WebMvcConfigurerAdapter可以来扩展SpringMVC的功能
-//@EnableWebMvc   不要接管SpringMVC
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         // super.addViewControllers(registry);
         //浏览器发送 /atguigu 请求来到 success
         registry.addViewController("/atguigu").setViewName("success");
     }
-    //所有的WebMvcConfigurerAdapter组件都会一起生效
-    @Bean
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
-        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("login");
-                registry.addViewController("/index.html").setViewName("login");
-            }
-        };
-        return adapter;
-    }
-
 }
--- 01-atguigui-parent/spring-boot-04-webs/spring-boot-04-web-webbase/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ 01-atguigui-parent/spring-boot-04-webs/spring-boot-04-web-webbase/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,24 +1,17 @@
 package com.atguigu.springboot.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.Map;
 
-/**
- * @author Chen
- * @created 2018-10-31-10:37.
- */
 @Controller
 public class HelloController {
 
-  /*  @RequestMapping({"/","/index.html"})
-    public String index(){
-        return "index";
-    }*/
 
     @ResponseBody
     @RequestMapping("/hello")
@@ -33,4 +26,6 @@ public class HelloController {
         map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success";
     }
+
+
 }
